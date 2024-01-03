417e909a682ae959770c1761c4b78ea51b765c8e
TamVanum <gaston.cath14@gmail.com>
2024-01-03 15:15:16 -0300

    fix: update table of especies in detalles

--- src/components/EstructuraProductiva/EspeciesCantidadForBackOffice.jsx
+++ src/components/EstructuraProductiva/EspeciesCantidadForBackOffice.jsx
@@ -4,21 +4,22 @@ import { DataGrid } from '@mui/x-data-grid';
 
 const EspeciesCantidadForBackOffice = ({ data }) => {
 
+  console.log(data)
   const headerAlignProps = {
     headerAlign: 'center',
     align: 'center',
   };
 
   const columns = [
-    { field: 'especieId', headerName: 'ID', flex: 1, headerAlign: 'center', align: 'center' },
-    { field: 'nombre', headerName: 'ID', flex: 1, headerAlign: 'center', align: 'center' },
-    { field: 'cantidad', headerName: 'ID', flex: 1, headerAlign: 'center', align: 'center' },
+    { field: 'codigo', headerName: 'codigo', flex: 1, headerAlign: 'center', align: 'center' },
+    { field: 'nombre', headerName: 'nombre', flex: 1, headerAlign: 'center', align: 'center' },
+    { field: 'cantidad', headerName: 'cantidad', flex: 1, headerAlign: 'center', align: 'center' },
     // Agrega más columnas según sea necesario
   ];
 
   const rows = data.map((item) => ({
     id: item.id,
-    especieId: item.especieId,
+    codigo: item.especie.codigo,
     nombre: item.especie.nombre,
     cantidad: item.cantidad,
     // Agrega más propiedades según sea necesario
